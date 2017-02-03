4e6464f5c87daf787811eebd0fd369c7d76a9cdb
Amanda Steckler <amandajsteckler@gmail.com>
2017-02-03 10:09:12 -0500

    Count letter occurences and put letters in array ordered most frequent to least frequent

--- min_key_presses.rb
+++ min_key_presses.rb
@@ -3,10 +3,33 @@ class MinKeyPresses
 
   def initialize(str)
     @str = str
+    @letter_count = {}
+    @optimized_keys = {}
   end
 
-  def remove_non_alphabetic
+  # remove non-alphabetic chars, put alphabetic chars into array
+  def alphabetic_chars
     non_alphabetic = @str.gsub(/[^a-zA-Z]/, '').downcase
     letter_array = non_alphabetic.split('')
   end
+
+  # count occurences of each letter and put in hash
+  def count_letters
+    alphabetic_chars.each do |letter|
+      if @letter_count[letter] != nil
+        @letter_count[letter] += 1
+      else
+        @letter_count[letter] = 1
+      end
+    end
+
+    @letter_count
+  end
+
+  # order the letters hash from most frequent to least
+  def ordered_letters
+    hash_in_order = count_letters.sort_by {|key, value| value}.reverse.to_h
+    @letters_in_order = hash_in_order.keys
+  end
+
 end
--- spec/min_key_presses_spec.rb
+++ spec/min_key_presses_spec.rb
@@ -2,6 +2,7 @@ require_relative '../min_key_presses'
 
 describe 'MinKeyPresses' do
   let(:typed_long) { MinKeyPresses.new("The quick brown fox jumps over the lazy dog.") }
+  let(:typed_medium) { MinKeyPresses.new("Medium string.") }
   let(:typed_short) { MinKeyPresses.new("Hi.") }
 
   describe '::new' do
@@ -11,9 +12,20 @@ describe 'MinKeyPresses' do
     end
   end
 
-  describe 'remove non-alphabetic characters' do
+  describe 'find alphabetic characters' do
     it 'removes non-alphabetic characters and puts alphabetic characters into array' do
-      expect(typed_short.remove_non_alphabetic).to eq(['h', 'i'])
+      expect(typed_short.alphabetic_chars).to eq(['h', 'i'])
     end
   end
+
+  describe 'count letters and put them in order' do
+    it 'counts the occurences of each letter and puts them in a hash' do
+      expect(typed_medium.count_letters).to eq({"m"=>2, "e"=>1, "d"=>1, "i"=>2, "u"=>1, "s"=>1, "t"=>1, "r"=>1, "n"=>1, "g"=>1})
+    end
+
+    it 'puts letters from most frequent to least' do
+      expect(typed_medium.ordered_letters).to eq(["m", "i", "t", "s", "u", "n", "r", "d", "e", "g"])
+    end
+  end
+
 end
